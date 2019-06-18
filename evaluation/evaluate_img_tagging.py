55de4786999c6dbb75d04fb0d0b0e67405cbd74c
raulgomez <raulgombru@gmail.com>
2019-06-18 10:45:54 +0200

    Text Style Transfer

--- evaluation/evaluate_img_tagging.py
+++ evaluation/evaluate_img_tagging.py
@@ -12,7 +12,7 @@ import json
 import numpy as np
 
 dataset = '../../../hd/datasets/YFCC100M/'
-model_name = 'YFCC_NCSL_epoch_15_ValLoss_0.42.pth'
+model_name = 'YFCC_NCSL_epoch_15_ValLoss_0.42'
 test_split_path = '../../../datasets/YFCC100M/splits/test.txt'
 img_embeddings_path = dataset + 'results/' + model_name + '/images_test.json'
 # tags_embeddings_path = dataset + 'results/' + model_name + '/tags.json'
